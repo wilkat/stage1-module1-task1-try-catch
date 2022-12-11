aa9624d265674742a60953c6420ce3a9ed68d3eb
wilkat <katarzyna.f.wilk@gmail.com>
2022-12-11 19:15:23 +0100

    new

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -1,8 +1,11 @@
 package com.epam.m1.exceptions;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Complete the code, parse integers, calculate the sum of numbers in the WORDS, join strings with
@@ -12,18 +15,29 @@ public class ParseIntegers {
 
     private static final List<String> WORDS =
             Arrays.asList(
-                    "JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations."
-                            .split(" "));
+                    "JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations.".split(" "));
 
     public static void main(String[] args) {
         Iterator<String> words = WORDS.iterator();
         int sum = 0;
         String justWords = "";
+        List<String> onlyWords = new ArrayList<>();
+        Pattern integerPattern = Pattern.compile("-?\\d+");
+
         while (words.hasNext()) {
             String next = words.next();
-            int number = Integer.parseInt(next);
-            // todo: complete it
+            int number;
+            Matcher matcher = integerPattern.matcher(next);
+            boolean matches = matcher.matches();
+            if(matches) {
+                number = Integer.parseInt(next);
+                sum += number;
+            } else {
+                onlyWords.add(next);
+            }
         }
+        justWords = String.join(" ", onlyWords);
+
         System.out.println("Sum is " + sum);
         System.out.println("Just words:" + justWords);
     }
